9a4b50344188e1a2088fa854e05adf087bfd64fa
Meagan Beckstrand <mbeckstrand101@yahoo.com>
2024-01-30 19:00:15 -0800

    edit graph labels and function arguments

--- basicgraph.py
+++ basicgraph.py
@@ -2,15 +2,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def basic_graph(output_dict, options_dict):
+
+def basic_graph(output_dict, course_mode, options_dict):
     #Extract graph data from output_dict
     x_data = list(output_dict.keys()) #get percentage data from dict
     y_data = list(output_dict.values()) #get professor or class data from dict
 
     #Read labels for graph
-    title = options_dict["TITLE"] 
-    y_label = options_dict["PROF"]
-    x_label = options_dict["per"]
+    title = course_mode
+    y_label = options_dict["grade_mode"]
+    x_label = "Instructor"
+    
+    #if showing class count
+    if options_dict["show_class_count"] == True:
+        x_label += "(and number of classes taught)"
+    else if options_dict["show_all_classes"] == True:
+        x_label = "Class"
+
 
     #Assign figure size
     fig = plt.figure(figsize = (10, 5))
