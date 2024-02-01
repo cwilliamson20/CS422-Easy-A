26c0e44836dd875d2598eab01032da4d2de9ee9c
Admin <etiennec@uoregon.edu>
2024-01-31 20:25:46 -0800

    setup data verification argument

---
+++ admin.py
@@ -0,0 +1,34 @@
+# created by Etienne
+
+
+
+import sys
+import verify_data
+
+
+
+
+
+
+def main():
+
+    print("usage: ...")
+    print(sys.argv)
+    if sys.argv[1] == "--verify" or sys.argv[1] == "-V":
+        verify_data.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
