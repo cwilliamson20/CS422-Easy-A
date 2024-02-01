fe25d4ae1cc45c03c6dd223735b0f2679b159c58
Admin <etiennec@uoregon.edu>
2024-02-01 15:29:16 -0800

    added changing databases to admin mode

--- admin.py
+++ admin.py
@@ -4,6 +4,9 @@
 
 import sys
 import verify_data
+import os
+import shutil
+import discresolution
 
 
 def print_usage():
@@ -13,7 +16,6 @@ def print_usage():
 
 
 def main():
-
     if len(sys.argv) < 2:
         print_usage()
     elif sys.argv[1] == "--verify" or sys.argv[1] == "-V":
@@ -23,9 +25,24 @@ def main():
         if len(sys.argv) < 3:
             print("Usage: python3 admin.py -r <filename>")
         else:
-            print(f"Replacing with file {sys.argv[2]}")
-    elif sys.argv[1] == "-webscrapper" or sys.argv[1] == "-w":
+            if os.path.exists("data.csv"):
+                print("Backing up file data.csv to data.csv.tmp")
+                shutil.copyfile("data.csv", "data.csv.tmp")
+
+
+            print(f"Replacing data.csv with file {sys.argv[2]}")
+            shutil.copyfile(sys.argv[2], "data.csv")
+            print("Creating backup file data.csv.old")
+            os.replace("data.csv.tmp","data.csv.old")
+            print("done successfully")
+
+
+
+
+
+    elif sys.argv[1] == "--webscrapper" or sys.argv[1] == "-w":
         print("Running the webscraper and updating faculty list!")
+
     else:
         print("Invalid arguments!")
         print_usage()
