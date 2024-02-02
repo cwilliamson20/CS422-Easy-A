019fbf920d941703c3cb8cbce6319bfabd0dcdf0
Meagan Beckstrand <mbeckstrand101@yahoo.com>
2024-02-02 15:20:59 -0800

    added comments

--- basicgraph.py
+++ basicgraph.py
@@ -4,7 +4,14 @@ import numpy as np
 
 
 def basic_graph(output_dict,options_dict):
-    
+    """
+    Take the grade data after it has been parsed through and generate a graph.
+
+    Args:
+        output_dict (dict): The dictionary containing the grade data, including professor name or class number, and grade the percentage. 
+        options_dict (dict): The dictionary containing the selections of the user, used for labeling the graph
+
+    """
     #Sort the data from output_dict in descending order
     sorted_dict = sorted(output_dict.items(), key = lambda x: x[1], reverse = True)}
     
@@ -13,16 +20,19 @@ def basic_graph(output_dict,options_dict):
     #Get graph data on grade percentages
     y_data = list(sorted_dict.values()) 
 
+    #Determine the number of entries in graph
+    entries = 5
+    
     #Get the top 5 data entries by percentage
-    y_data = y_data[:4]
-    x_data = x_data[:4]
+    y_data = y_data[:entries]
+    x_data = x_data[:entries]
     
     #Read labels for graph
     title = options_dict["course_mode"]
     y_label = options_dict["grade_mode"]
     x_label = options_dict["x_axis"]
     
-    #if showing class count
+    #if showing class count, update x label
     if options_dict["show_class_count"] == True:
         x_label += "(and number of classes taught)"
 
@@ -33,8 +43,7 @@ def basic_graph(output_dict,options_dict):
     #Bar plot
     plt.bar(x_data, y_data, color = blue, width = 1, align='center' ) 
     plt.ylim(0, 100)
-    # Add gridlines
-    #plt.grid(b = True, color = 'grey', linestyle = '-.', linewidth = 0.5, alpha = 0.2)
+    
     #Rotate x labels
     plt.xticks(rotation=90) 
 
@@ -46,9 +55,3 @@ def basic_graph(output_dict,options_dict):
     #Generate graph
     plt.show()
 
-
-#def main():
-#    basicgraph():
-
-if __name__ == "__main__":
-    main()
