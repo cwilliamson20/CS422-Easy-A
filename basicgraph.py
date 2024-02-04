1ad53f16fa06147a16024d3253e897ffed0d5381
Meagan Beckstrand <mbeckstrand101@yahoo.com>
2024-02-04 13:43:46 -0800

    edited graph appearance

--- basicgraph.py
+++ basicgraph.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import matplotlib.pyplot as plt
-import numpy as np
+#import numpy as np
 
 
 def basic_graph(output_dict,options_dict):
@@ -12,13 +12,19 @@ def basic_graph(output_dict,options_dict):
         options_dict (dict): The dictionary containing the selections of the user, used for labeling the graph
 
     """
+    if not output_dict:
+        print("Error getting graph data!")
+        return
+
+
     #Sort the data from output_dict in descending order
-    sorted_dict = sorted(output_dict.items(), key = lambda x: x[1], reverse = True)
+    tupled_output = list(output_dict.items())
+    sorted_dict = sorted(tupled_output, key = lambda x: x[1], reverse = True)
     
     #Get graph data on professor/class names
-    x_data = list(sorted_dict.keys()) 
+    x_data = [x[0] for x in sorted_output]
     #Get graph data on grade percentages
-    y_data = list(sorted_dict.values()) 
+    y_data = [x[1] for x in sorted_output]
 
     #Determine the number of entries in graph
     entries = 5
@@ -28,7 +34,7 @@ def basic_graph(output_dict,options_dict):
     x_data = x_data[:entries]
     
     #Read labels for graph
-    title = options_dict["course_mode"]
+    fig_title = options_dict["course_mode"]
     y_label = options_dict["grade_mode"]
     x_label = options_dict["x_axis"]
     
@@ -38,19 +44,23 @@ def basic_graph(output_dict,options_dict):
 
 
     #Assign figure size
-    fig = plt.figure(figsize = (10, 5))
+    fig, ax = plt.figure(figsize = (10, 5))
     
     #Bar plot
-    plt.bar(x_data, y_data, color = blue, width = 1, align='center' ) 
-    plt.ylim(0, 100)
+    ax.bar(x_data, y_data, color = 'blue', width=0.7, align='center')
+    ax.set_xlim(-0.5, entries)
+    ax.set_ylim(0,100)
+   
+    #Add labels
+    ax.set(ylabel=y_label, xlabel=x_label, title=fig_title)
     
     #Rotate x labels
-    plt.xticks(rotation=90) 
+    plt.xticks(rotation=30, ha='center') 
 
     #Add labels
-    plt.ylabel(y_label) 
-    plt.xlabel(x_label) 
-    plt.title(title)
+    #plt.ylabel(y_label) 
+    #plt.xlabel(x_label) 
+    #plt.title(title)
 
     #Generate graph
     plt.show()
