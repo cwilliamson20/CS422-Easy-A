612ded60bdb1338806637f6e2a704755c159a9ca
elizagb <egerblack@gmail.com>
2024-02-05 10:41:45 -0800

    adding comments

--- Source Code/user_create_graph.py
+++ Source Code/user_create_graph.py
@@ -2,15 +2,10 @@
 # Author(s): Meagan Beckstrand, Eliza Black, Connie Williamson
 # Group 4
 # Created 1/29/2024
-# Date Last Modified: 2/4/2024
+# Date Last Modified: 2/5/2024
 import gen_data
 import basicgraph
 
-"""
-TODO: 
-- send course_mode as parameter of basicgraph
-basicgraph(output_dict, course_mode, options_dict)
-"""
 
 def get_graph_data(app, root):
 
@@ -19,16 +14,15 @@ def get_graph_data(app, root):
     print(f"options_dict = {options_dict}")
 
     # Unpacking dictionary values to get graph option values:
-
     subject_code = options_dict["subject_code"]
     course_number = options_dict["course_number"]
 
-    # course_number = 210
-
+    # Renaming "All" year selection so it is compatible with gen_data functionality
     year = options_dict["year"]
     if year == "All":
         year = "*"
 
+    # Reformatting graph_type input using Enum values compatible with gen_data.py
     course_mode = 0
     if options_dict["graph_type"] == 1:
         course_mode = gen_data.Course_Data_Mode.SINGLE_COURSE
@@ -38,13 +32,14 @@ def get_graph_data(app, root):
         course_mode = gen_data.Course_Data_Mode.COURSE_LEVEL
 
     # TODO: add options for %Cs, etc
+    # Reformatting grade_mode input using Enum values compatible with gen_data.py
     grade_mode = 0
     if options_dict["grade_mode"] == 1:
         grade_mode = gen_data.Value_Data_Mode.PERC_AS
     elif options_dict["grade_mode"] == 2:
         grade_mode = gen_data.Value_Data_Mode.PERC_D_AND_PERC_F
 
-    # add information about class or instructor level
+    # Reformatting x_axis input to make compatible with gen_data.py
     x_axis_type = options_dict["x_axis"]
     if x_axis_type == 1:
         x_axis_type = "Class"
