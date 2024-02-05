011fee0b339415d5627ba72f43f8b21f734243f0
Admin <etiennec@uoregon.edu>
2024-02-05 14:55:18 -0800

    added comments for gen_data.py

--- Source Code/gen_data.py
+++ Source Code/gen_data.py
@@ -15,16 +15,17 @@ class Course_Data_Mode(Enum):
 # see gen_data documentation
 class Value_Data_Mode(Enum):
     PERC_AS= "aprec"
-    PERC_BS="bprec"
-    PERC_CS="cprec"
-    PERC_DS="dprec"
-    PERC_FS="fprec"
+    # PERC_BS="bprec"
+    # PERC_CS="cprec"
+    # PERC_DS="dprec"
+    # PERC_FS="fprec"
     PERC_D_AND_PERC_F="D/F"
 
 
 
 #helper function for gen_data
 def proc_row(row: dict, mode: Value_Data_Mode):
+    # Either take the %As from a course or combine the % Ds and Fs from a course.
     if mode is not Value_Data_Mode.PERC_D_AND_PERC_F:
         return float(row[mode.value])
     elif mode is Value_Data_Mode.PERC_D_AND_PERC_F:
@@ -35,6 +36,7 @@ def proc_row(row: dict, mode: Value_Data_Mode):
 # subject - the subject of the course. For example MATH
 # course -  the subject code for the course. For example 251, or the entire department level (100 to 400) 
 # mode - the data criteria mode. For example SINGLE_COURSE
+#    If you choose mode SINGLE_COURSE, then only the course code will be matched
 #    If you choose mode COURSE_LEVEL, then the first digit of the string will determine the level
 #    If you choose mode DEPARTMENT, then the value of course will be ignored
 # year - the years you want to search. For example, "2015 2016"
@@ -42,6 +44,7 @@ def proc_row(row: dict, mode: Value_Data_Mode):
 #   multiple years (e.g. "2014 2016"),
 #   or an asterix for all years (e.g. "*.")
 # values -  what values do you want to retrieve. What do tou want to measure? For example, PERC_AS
+#   PERC_AS will only take percent As
 #   PERC_D_AND_PERC_F will compound the nonpassing grades D + F
 # output_mode - what you want the keys in the return dict to be. Either "Instructor" or "Class".
 # show_nums - put the number of coures in the key for the dictionary in the format PROF_NAME (NUM). Either True or False (default)
@@ -55,8 +58,11 @@ def gen_data(subject: str, course: int, mode: Course_Data_Mode, year: str, value
     ctr_dict = {}
     reg_fac = []
 
+
+    # check to see if output.csv exists and if it does build the regular faculty dictionary
     if reg_fac_only:
         try:
+            # open file and use python csv import to take care of reading csv file
             with open('output.csv', mode= "r", encoding="latin-1") as csvfile:
                 reader = csv.DictReader(csvfile)
                 for row in reader:
@@ -67,6 +73,7 @@ def gen_data(subject: str, course: int, mode: Course_Data_Mode, year: str, value
             print(f"Faculty list retrieval error. Couldn't read output.csv ({e}). Have you run the webscraper?")
             print("Proceeding as if all faculty is selected. ")
     # print(reg_fac)
+    # open file and use python csv import to take care of reading csv file
     with open('data.csv', mode= "r", encoding="utf-8-sig") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
@@ -74,12 +81,16 @@ def gen_data(subject: str, course: int, mode: Course_Data_Mode, year: str, value
             # print(row["TERM"][:4] +" is "+ year)
             # break
             # if row["TERM"][:4] == year:
+
+            # check for the year
             if year in row["TERM_DESC"] or year == "*":
                 # print("year pass")
                 # print(row["SUBJ"] +"is" +subject)
+                # check for subject
                 if row["SUBJ"] == subject:
                     # print("subj pass")
                     # print("MATCH!" + row["SUBJ"] +":" +row["NUMB"])
+                    #check the data mode if it does not meet criteria move on to next entry
                     if mode == Course_Data_Mode.SINGLE_COURSE:
                         if row["NUMB"] != course:
                             continue # does not meet criteria
@@ -90,12 +101,14 @@ def gen_data(subject: str, course: int, mode: Course_Data_Mode, year: str, value
                         continue # neither of the above
 
                     prof = row["INSTRUCTOR"]
-
+                    # use the data from output.csv
                     if reg_fac_only:
                         # splits  = prof.split()
                         # if splits[0][-1] == ',':
                         #     splits[0] = splits[0][:-1]
                         b = False
+                        # try to match the prof name for the course and find it in the CSV. if we cant find it then move
+                        # onto next course
                         for s in reg_fac:
                             splits2 = s.split()
                             # print(splits)
@@ -109,29 +122,35 @@ def gen_data(subject: str, course: int, mode: Course_Data_Mode, year: str, value
                         if not b:
                             continue
 
-                    # meets criteria
+                    # if we get to this point then we meet the criteria
+
+
+                    # store class name if this mode is selected
                     if output_mode == "Class":
                         prof = row["NUMB"]
 
 
                     
                     print("storing info on Prof. " + prof +" for course "+row["SUBJ"]+" "+row["NUMB"]+" for "+row["TERM_DESC"])
+                    # if its not in the dict than put it in
                     if prof not in output_dict:
                         output_dict[prof] = 0.0
                         ctr_dict[prof] = 0
+                    # append the value and counter (for average calc)
                     ctr_dict[prof] += 1
                     output_dict[prof] += proc_row(row, values)
 
 
-
+    # compute the average for each prof name / course numb
     for key in output_dict:
         output_dict[key] = output_dict[key] / ctr_dict[key]
 
     print(f"output_dict: {output_dict}")
+    # if not show nums then just return. we're done
     if not show_nums:
         return output_dict
     outdict_2 = {}
-
+    #otherwise add the (X) to each key and return that
     for key in output_dict:
         outdict_2[key + f" ({ctr_dict[key]})"] = output_dict[key]
     
